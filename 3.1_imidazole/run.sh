baba36928ee067518573c3c6561f3a2dcb2d5ed8
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2021-06-07 14:58:49 +0900

    run in SailingShip

--- 3.1_imidazole/run.sh
+++ 3.1_imidazole/run.sh
@@ -2,7 +2,11 @@
 
 MOL=imidazole
 export GAUSS_SCRDIR=/scr/$USER/$MOL.$$
+mkdir -p $GAUSS_SCRDIR
+
 (time g16 < $MOL.com) > $MOL.out 2>&1
+rm -r $GAUSS_SCRDIR
+
 formchk $MOL.chk
 cubegen 0 Density=SCF   $MOL.fchk $MOL.density.cub 0 h
 cubegen 0 Potential=SCF $MOL.fchk $MOL.esp.cub     0 h
