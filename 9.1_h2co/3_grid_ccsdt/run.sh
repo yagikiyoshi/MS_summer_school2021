8dcd40e3365e06bb53d80696c7de572c497b07bf
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2021-07-02 15:28:14 +0900

    update 9.1

---
+++ 9.1_h2co/3_grid_ccsdt/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+module load gaussian
+. /home1/common/sindo/sindovars.sh
+
+if [ -e resources.info ]; then
+  rm resources.info
+fi
+
+hostname >> resources.info
+hostname >> resources.info
+
+java RunMakePES -f makePES.xml >& makePES.out
+
--- 9.1_h2co/4_vib/run.sh
+++ 9.1_h2co/4_vib/run.sh
@@ -3,14 +3,23 @@
 . /home1/common/sindo/sindovars.sh
 
 level=b3lyp
-export POTDIR=../3_mrpes/pes_${level}
+export POTDIR=pes_${level}
+mkdir  ${POTDIR}
+cp ../2_qff/prop_no_1.mop  ${POTDIR}
+cp ../3_grid_b3lyp/*pot    ${POTDIR}
+cp ../3_grid_b3lyp/*dipole ${POTDIR}
 
 sindo < vci.inp    > vci_${level}.out    2>&1
 mv vci-IR.data vci-IR_${level}.data
 mv vci-IR.spectrum vci-IR_${level}.spectrum
 
 level=mrpes
-export POTDIR=../3_mrpes/pes_${level}
+export POTDIR=pes_${level}
+mkdir  ${POTDIR}
+cp ../2_qff/prop_no_1.mop  ${POTDIR}
+cp ../3_grid_b3lyp/*pot    ${POTDIR}
+cp ../3_grid_b3lyp/*dipole ${POTDIR}
+cp ../3_grid_ccsdt/*pot    ${POTDIR}
 
 sindo < vci.inp    > vci_${level}.out    2>&1
 mv vci-IR.data vci-IR_${level}.data
